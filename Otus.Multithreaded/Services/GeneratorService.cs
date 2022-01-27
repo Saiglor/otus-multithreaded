e443ebedd38a70abf63c5ffa9c85ae2ad94f9eaa
saiglor <jakimest.2012@gmail.com>
2022-01-27 13:48:17 +0500

    Получение списка

---
+++ Otus.Multithreaded/Services/GeneratorService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Otus.Multithreaded.Services
+{
+    public static class GeneratorService
+    {
+        public static List<int> GenerateIntList(int count)
+        {
+            var rnd = new Random();
+            var res = new List<int>();
+
+            for (var i = 0; i < count; i++)
+            {
+                res.Add(rnd.Next(0, 100));
+            }
+
+            return res;
+        }
+    }
+}
