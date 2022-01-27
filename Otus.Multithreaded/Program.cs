fae27db9a8ab6676bddfed390b18898d4c681d69
saiglor <jakimest.2012@gmail.com>
2022-01-27 13:48:37 +0500

    Вывод списка и времени выполнения

---
+++ Otus.Multithreaded/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Otus.Multithreaded.Services;
+
+
+namespace Otus.Multithreaded
+{
+    class Program
+    {
+        private static readonly int[] _counts = {100000, 1000000, 10000000};
+
+        private static void Main()
+        {
+            foreach (var count in _counts)
+            {
+                var list = GeneratorService.GenerateIntList(count);
+
+                WriteLine(list);
+                WriteLineParallel(list);
+                WriteLineParallelLinq(list);
+
+                Console.WriteLine();
+            }
+        }
+
+        private static void WriteLine(List<int> list)
+        {
+            var stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+
+            var sum = new SumService().GetSum(list);
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"Обычное: {sum}; Milliseconds: {stopwatch.ElapsedMilliseconds}");
+        }
+
+        private static void WriteLineParallel(List<int> list)
+        {
+            var stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+
+            var sum = new ParallelSumService().GetSum(list);
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"Параллельное: {sum}; Milliseconds: {stopwatch.ElapsedMilliseconds}");
+        }
+
+        private static void WriteLineParallelLinq(List<int> list)
+        {
+            var stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+
+            var sum = new ParallelLinqSumService().GetSum(list);
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"Параллельное с помощью LINQ: {sum}; Milliseconds: {stopwatch.ElapsedMilliseconds}");
+        }
+    }
+}
